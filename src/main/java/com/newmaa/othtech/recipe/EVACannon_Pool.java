db3726a6238fa634119c01b3e17ace90e9574cde
NewMaa <slmajlapeng@163.com>
2024-09-07 23:21:52 +0800

    sd

--- src/main/java/com/newmaa/othtech/recipe/EVACannon_Pool.java
+++ src/main/java/com/newmaa/othtech/recipe/EVACannon_Pool.java
@@ -2,12 +2,26 @@ package com.newmaa.othtech.recipe;
 
 import com.newmaa.othtech.common.recipemap.Recipemaps;
 
+import gregtech.api.enums.GT_Values;
 import gregtech.api.recipe.RecipeMap;
+import gregtech.api.util.GT_Utility;
 
 public class EVACannon_Pool implements IRecipePool {
 
     @Override
     public void loadRecipes() {
         RecipeMap<?> EVA = Recipemaps.Cannon;
+        GT_Values.RA.stdBuilder()
+            .itemInputs(GT_Utility.getIntegratedCircuit(1))
+            .noOptimize()
+            .eut(2147483647)
+            .duration(1)
+            .addTo(EVA);
+        GT_Values.RA.stdBuilder()
+            .itemInputs(GT_Utility.getIntegratedCircuit(2))
+            .noOptimize()
+            .eut(2147483647)
+            .duration(65536)
+            .addTo(EVA);
     }
 }
