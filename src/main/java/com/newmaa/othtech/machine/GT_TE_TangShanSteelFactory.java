f03bc30e265d66780f37fa282fa0b8f1a97f8b84
NewMaa <slmajlapeng@163.com>
2024-09-24 22:06:50 +0800

    sdasd

--- src/main/java/com/newmaa/othtech/machine/GT_TE_TangShanSteelFactory.java
+++ src/main/java/com/newmaa/othtech/machine/GT_TE_TangShanSteelFactory.java
@@ -1286,7 +1286,7 @@ public class GT_TE_TangShanSteelFactory extends OTH_MultiMachineBase<GT_TE_TangS
             .addInfo("正常耗时倍率 : 1 / 线圈等级")
             .addInfo("无线耗时 = 240s / 编程电路编号 ^ 2)")
             .addInfo("无线EU消耗 = 功率 * 耗时 * 编程电路编号 , 单次最高消耗8E EU")
-            .addInfo("请注意炉温要求")
+            .addInfo("请注意炉温要求.")
             .addInfo("螺丝刀切换无线模式")
             .addInfo("非无线模式执行无损超频")
             .addInfo("§q支持§bTecTech§q能源仓及激光仓，同时支持无线电网直接供给EU")
