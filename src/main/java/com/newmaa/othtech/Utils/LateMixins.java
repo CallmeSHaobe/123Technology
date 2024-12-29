3f2ff7cb53e7c4077ac93b8bc2d481c608c891bb
Safari_Xiu <xiusafari@gmail.com>
2024-12-29 15:49:51 +0800

    添加Mixin插件 对MultiBlockBase中的污染处理部分进行了覆写

---
+++ src/main/java/com/newmaa/othtech/Utils/LateMixins.java
@@ -0,0 +1,28 @@
+package com.newmaa.othtech.Utils;
+
+import com.gtnewhorizon.gtnhmixins.ILateMixinLoader;
+import com.gtnewhorizon.gtnhmixins.LateMixin;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+
+@LateMixin
+public class LateMixins implements ILateMixinLoader {
+
+    @Override
+    public String getMixinConfig() {
+        return "mixins.othtech.late.json";
+    }
+
+    @Override
+    public List<String> getMixins(Set<String> loadedMods) {
+        // 创建 Mixin 列表
+        List<String> mixins = new ArrayList<>(Arrays.asList(
+            "MTEMultiBlockBaseMixin"
+        ));
+
+        return mixins;
+    }
+}
