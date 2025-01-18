7107a188b08e9a1f57f125b7ec008cf834e4120e
NewMaa <slmajlapeng@163.com>
2025-01-18 20:42:31 +0800

    BUILD 2025-1-18 4

--- src/main/java/com/newmaa/othtech/common/blocks/modelAyanami.java
+++ src/main/java/com/newmaa/othtech/common/blocks/modelAyanami.java
@@ -50,6 +50,7 @@ public class modelAyanami extends BaseBlockContainer implements IRegistry<modelA
     public TileEntity createNewTileEntity(World worldIn, int meta) {
         return new tileEntityModelAyanami();
     }
+
     @Override
     public void onBlockPlacedBy(World world, int x, int y, int z, EntityLivingBase player, ItemStack itemstack) {
         int l = MathHelper.floor_double(player.rotationYaw * 4.0F / 360.0F + 0.5D) & 3;
