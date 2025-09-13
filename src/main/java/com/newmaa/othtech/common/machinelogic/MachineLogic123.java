538aa4b58936846cc9a10bdbd7a9b9259957dc84
czqwq <87927909+czqwq@users.noreply.github.com>
2025-09-13 16:44:11 +0800

    Update MachineLogic123.java

--- src/main/java/com/newmaa/othtech/common/machinelogic/MachineLogic123.java
+++ src/main/java/com/newmaa/othtech/common/machinelogic/MachineLogic123.java
@@ -10,16 +10,9 @@ import gregtech.api.util.GTRecipe;
 import gregtech.api.util.ParallelHelper;
 
 public class MachineLogic123 extends ProcessingLogic {
-
-    // 默认超频参数
-    private int defaultTimeReduction = 2;
-    private int defaultPowerIncrease = 4;
-    private boolean defaultOverclockApplied = false;
-
     @NotNull
     @Override
     protected ParallelHelper createParallelHelper(@Nonnull GTRecipe recipe) {
-        // @formatter:off
         return new ParallelHelper().setRecipe(recipe)
             .setItemInputs(inputItems)
             .setFluidInputs(inputFluids)
@@ -31,28 +24,5 @@ public class MachineLogic123 extends ProcessingLogic {
             .enableBatchMode(batchSize)
             .setConsumption(true)
             .setOutputCalculation(true);
-        // @formatter:on
-    }
-
-    @NotNull
-    @Override
-    public CheckRecipeResult process() {
-        // 如果尚未应用默认超频，则应用默认设置
-        if (!defaultOverclockApplied) {
-            setOverclock(defaultTimeReduction, defaultPowerIncrease);
-            defaultOverclockApplied = true;
-        }
-        return super.process();
-    }
-
-    /**
-     * 设置默认超频类型
-     * 注意：外部调用 setOverclock 会覆盖此设置
-     */
-    public MachineLogic123 setDefaultOverclockType(int timeReduction, int powerIncrease) {
-        this.defaultTimeReduction = timeReduction;
-        this.defaultPowerIncrease = powerIncrease;
-        this.defaultOverclockApplied = false; // 重置标志，以便下次处理时应用新设置
-        return this;
     }
 }
