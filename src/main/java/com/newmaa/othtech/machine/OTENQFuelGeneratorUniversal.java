15ce33740dec189571daf3dd65c07fdd5be585ab
czqwq <18707538395@163.com>
2025-09-21 04:17:13 +0800

    spotless

--- src/main/java/com/newmaa/othtech/machine/OTENQFuelGeneratorUniversal.java
+++ src/main/java/com/newmaa/othtech/machine/OTENQFuelGeneratorUniversal.java
@@ -40,8 +40,8 @@ import com.gtnewhorizon.structurelib.alignment.constructable.ISurvivalConstructa
 import com.gtnewhorizon.structurelib.structure.IStructureDefinition;
 import com.gtnewhorizon.structurelib.structure.ISurvivalBuildEnvironment;
 import com.gtnewhorizon.structurelib.structure.StructureDefinition;
-import com.newmaa.othtech.common.machinelogic.TTMachinelogic123;
 import com.newmaa.othtech.common.materials.BWLiquids;
+import com.newmaa.othtech.machine.machineclass.TTMultiMachineBaseEM;
 
 import gregtech.api.interfaces.ITexture;
 import gregtech.api.interfaces.metatileentity.IMetaTileEntity;
@@ -70,7 +70,8 @@ import tectech.thing.metaTileEntity.multi.base.IStatusFunction;
 import tectech.thing.metaTileEntity.multi.base.LedStatus;
 import tectech.thing.metaTileEntity.multi.base.Parameters;
 
-public class OTENQFuelGeneratorUniversal extends TTMachinelogic123 implements IConstructable, ISurvivalConstructable {
+public class OTENQFuelGeneratorUniversal extends TTMultiMachineBaseEM
+    implements IConstructable, ISurvivalConstructable {
 
     public OTENQFuelGeneratorUniversal(int aID, String aName, String aNameRegional) {
         super(aID, aName, aNameRegional);
@@ -82,7 +83,7 @@ public class OTENQFuelGeneratorUniversal extends TTMachinelogic123 implements IC
         super.useLongPower = true;
     }
 
-    private int pipeTier = 0;
+    private int pipeTier = 1; // 修改默认值为1而不是0
     private UUID ownerUUID;
     private boolean isWirelessMode = false;
     private String costingWirelessEU = "0";
@@ -103,6 +104,10 @@ public class OTENQFuelGeneratorUniversal extends TTMachinelogic123 implements IC
     @Override
     public void loadNBTData(final NBTTagCompound aNBT) {
         pipeTier = aNBT.getInteger("pipe");
+        // 兼容旧版本数据：如果pipeTier为0，则升级为1
+        if (pipeTier == 0) {
+            pipeTier = 1;
+        }
         isWirelessMode = aNBT.getBoolean("wireless");
         running_time = aNBT.getLong("Time");
         super.loadNBTData(aNBT);
@@ -508,7 +513,7 @@ public class OTENQFuelGeneratorUniversal extends TTMachinelogic123 implements IC
                         ofBlocksTiered(
                             OTENQFuelGeneratorUniversal::getPipeTier,
                             ImmutableList.of(Pair.of(sBlockCasings2, 15), Pair.of(sBlockCasings9, 14)),
-                            0,
+                            -1, // 将notSet值改为-1
                             (t, meta) -> t.pipeTier = meta,
                             t -> t.pipeTier)))
                 .addElement('C', ofBlock(sBlockCasings6, 8))
@@ -520,10 +525,10 @@ public class OTENQFuelGeneratorUniversal extends TTMachinelogic123 implements IC
     }
 
     public FluidStack getPromoter() {
-        if (pipeTier == 0) {
-            return BWLiquids.PromoterZPM.getFluidOrGas(1); // T1
-        } else if (pipeTier == 1) {
-            return BWLiquids.PromoterUEV.getFluidOrGas(1); // T2
+        if (pipeTier == 1) { // T1
+            return BWLiquids.PromoterZPM.getFluidOrGas(1);
+        } else if (pipeTier == 2) { // T2
+            return BWLiquids.PromoterUEV.getFluidOrGas(1);
         } else {
             return BWLiquids.PromoterZPM.getFluidOrGas(1); // fallback
         }
@@ -532,7 +537,7 @@ public class OTENQFuelGeneratorUniversal extends TTMachinelogic123 implements IC
     @Override
     public final void onScrewdriverRightClick(ForgeDirection side, EntityPlayer aPlayer, float aX, float aY, float aZ,
         ItemStack aTool) {
-        if (pipeTier == 1 && !isWirelessMode) { // 仅 T2 能开
+        if (pipeTier == 2 && !isWirelessMode) { // 仅 T2 能开
             isWirelessMode = true;
         } else {
             isWirelessMode = false;
@@ -546,9 +551,9 @@ public class OTENQFuelGeneratorUniversal extends TTMachinelogic123 implements IC
     protected static Integer getPipeTier(Block block, int meta) {
         if (block == null) return null;
         if (block == sBlockCasings2 && meta == 15) {
-            return 0; // T1
+            return 1; // T1 - 修改为1而不是0
         } else if (block == sBlockCasings9 && meta == 14) {
-            return 1; // T2
+            return 2; // T2 - 修改为2而不是1
         }
         return null;
     }
