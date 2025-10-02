6d73b91f2628be34f4f219af5f3681aba7966772
NewMaa <slmajlapeng@163.com>
2025-10-02 16:38:15 +0800

    2025-10-2 fix GC NASA Recipe Bugs und GTPP RecipeMap bugs in map registering

--- src/main/java/com/newmaa/othtech/OTHTechnology.java
+++ src/main/java/com/newmaa/othtech/OTHTechnology.java
@@ -61,7 +61,7 @@ public class OTHTechnology {
 
     public static final String modName = Tags.MODNAME;
 
-    public static final String VERSION = "2.1.2";
+    public static final String VERSION = "2.1.3";
 
     public static final String Arthur = "Laodengs";
 
